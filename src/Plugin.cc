0c088e86b4dacd6973ecfd91abacf44ddbb6503b
Muhammad Saboor <muhammad.saboor@trapmine.com>
2021-10-07 03:31:16 -0700

    Update plugin description

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -9,7 +9,7 @@ zeek::plugin::Configuration Plugin::Configure()
 	{
 	zeek::plugin::Configuration config;
 	config.name = "Trapmine::AMQPWriter";
-	config.description = "<Insert description>";
+	config.description = "Sends logs to AMQP Exchange";
 	config.version.major = 0;
 	config.version.minor = 1;
 	config.version.patch = 0;
